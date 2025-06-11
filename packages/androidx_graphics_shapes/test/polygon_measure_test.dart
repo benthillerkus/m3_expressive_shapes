87f7cd80d1535698f6625efbe06055654dd9c4c0
Bent Hillerkus <bent@bent.party>
2025-06-11 17:24:47 +0200

    feat: finish porting polygon measure test

--- packages/androidx_graphics_shapes/test/polygon_measure_test.dart
+++ packages/androidx_graphics_shapes/test/polygon_measure_test.dart
@@ -1,11 +1,15 @@
 // Ported from PolygonMeasureTest.kt in https://cs.android.com/androidx/platform/frameworks/support/+/androidx-main:graphics/graphics-shapes/src/androidInstrumentedTest/kotlin/androidx/graphics/shapes/PolygonMeasureTest.kt
 // See original license at the end of this file.
 
+import 'dart:math';
 import 'dart:typed_data';
 
 import 'package:androidx_graphics_shapes/corner_rounding.dart';
+import 'package:androidx_graphics_shapes/cubic.dart';
+import 'package:androidx_graphics_shapes/features.dart';
 import 'package:androidx_graphics_shapes/polygon_measure.dart';
 import 'package:androidx_graphics_shapes/rounded_polygon.dart';
+import 'package:collection/collection.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'utils.dart';
@@ -45,14 +49,86 @@ void main() {
     // White box test: As the length measurer approximates arcs by linear segments,
     // this test validates if the chosen segment count approximates the arc length up to         // an error of 1.5% from the true length
     const vertices = 4;
-    // final polygon = RoundedPolygon.circle(numVertices = vertices);
+    final polygon = RoundedPolygon.circle(numVertices: vertices);
 
-    throw UnimplementedError("requires shapes");
+    final actualLength = polygon.cubics.map((it) => const LengthMeasurer().measureCubic(it)).sum;
+    final expectedLength = 2 * pi;
 
-    // val actualLength = polygon.cubics.sumOf { LengthMeasurer().measureCubic(it).toDouble() }
-    // val expectedLength = 2 * PI
+    expect(actualLength, closeTo(actualLength, 0.015 * expectedLength));
+  });
 
-    // assertEquals(expectedLength, actualLength, 0.015f * expectedLength)
+  test("irregular triangle angle measure", () {
+    irregularPolygonMeasure(
+      RoundedPolygon.fromVertices(
+        Float32List.fromList([0, -1, 1, 1, 0, 0.5, -1, 1]),
+        perVertexRounding: [
+          const CornerRounding(radius: .2, smoothing: .5),
+          const CornerRounding(radius: .2, smoothing: .5),
+          const CornerRounding(radius: .4, smoothing: 0),
+          const CornerRounding(radius: .2, smoothing: .5),
+        ],
+      ),
+    );
+  });
+
+  test("quarter angle measure", () {
+    irregularPolygonMeasure(
+      RoundedPolygon.fromVertices(
+        Float32List.fromList([-1, -1, 1, -1, 1, 1, -1, 1]),
+        perVertexRounding: [
+          CornerRounding.unrounded,
+          CornerRounding.unrounded,
+          const CornerRounding(radius: .5, smoothing: .5),
+          CornerRounding.unrounded,
+        ],
+      ),
+    );
+  });
+
+  test("hour glass measure", () {
+    // Regression test: Legacy measurer (AngleMeasurer) would skip the diagonal sides
+    // as they are 0 degrees from the center.
+    const unit = 1.0;
+    final coordinates = Float32List.fromList([
+      // lower glass
+      0, 0,
+      unit, unit,
+      -unit, unit,
+
+      // upper glass
+      0, 0,
+      -unit, -unit,
+      unit, -unit,
+    ]);
+
+    final diagonal = sqrt(unit * unit + unit * unit);
+    final horizontal = 2 * unit;
+    final total = 4 * diagonal + 2 * horizontal;
+
+    final polygon = RoundedPolygon.fromVertices(coordinates);
+    customPolygonMeasure(
+      polygon,
+      Float32List.fromList([
+        diagonal / total,
+        horizontal / total,
+        diagonal / total,
+        diagonal / total,
+        horizontal / total,
+        diagonal / total,
+      ]),
+    );
+  });
+
+  test("handles empty feature last", () {
+    final triangle = RoundedPolygon.fromFeatures([
+      Feature.buildConvexCorner([Cubic2D.straightLine(0, 0, 1, 1)]),
+      Feature.buildConvexCorner([Cubic2D.straightLine(1, 1, 1, 0)]),
+      Feature.buildConvexCorner([Cubic2D.straightLine(1, 0, 0, 0)]),
+      // Empty feature at the end.
+      Feature.buildConvexCorner([Cubic2D.straightLine(0, 0, 0, 0)]),
+    ]);
+
+    irregularPolygonMeasure(triangle);
   });
 }
 
@@ -141,200 +217,6 @@ void irregularPolygonMeasure(
 
   extraChecks?.call(measuredPolygon);
 }
-
-// package androidx.graphics.shapes
-
-// import androidx.test.filters.SmallTest
-// import kotlin.math.PI
-// import kotlin.math.sqrt
-// import org.junit.Assert.assertEquals
-// import org.junit.Assert.assertTrue
-// import org.junit.Test
-
-// @SmallTest
-// class PolygonMeasureTest {
-//     private val measurer = LengthMeasurer()
-
-//     @Test fun measureSharpTriangle() = regularPolygonMeasure(3)
-
-//     @Test fun measureSharpPentagon() = regularPolygonMeasure(5)
-
-//     @Test fun measureSharpOctagon() = regularPolygonMeasure(8)
-
-//     @Test fun measureSharpDodecagon() = regularPolygonMeasure(12)
-
-//     @Test fun measureSharpIcosagon() = regularPolygonMeasure(20)
-
-//     @Test
-//     fun measureSlightlyRoundedHexagon() {
-//         irregularPolygonMeasure(RoundedPolygon(6, rounding = CornerRounding(0.15f)))
-//     }
-
-//     @Test
-//     fun measureMediumRoundedHexagon() {
-//         irregularPolygonMeasure(RoundedPolygon(6, rounding = CornerRounding(0.5f)))
-//     }
-
-//     @Test
-//     fun measureMaximumRoundedHexagon() {
-//         irregularPolygonMeasure(RoundedPolygon(6, rounding = CornerRounding(1f)))
-//     }
-
-//     @Test
-//     fun measureCircle() {
-//         // White box test: As the length measurer approximates arcs by linear segments,
-//         // this test validates if the chosen segment count approximates the arc length up to
-//         // an error of 1.5% from the true length
-//         val vertices = 4
-//         val polygon = RoundedPolygon.circle(numVertices = vertices)
-
-//         val actualLength = polygon.cubics.sumOf { LengthMeasurer().measureCubic(it).toDouble() }
-//         val expectedLength = 2 * PI
-
-//         assertEquals(expectedLength, actualLength, 0.015f * expectedLength)
-//     }
-
-//     @Test
-//     fun irregularTriangleAngleMeasure() =
-//         irregularPolygonMeasure(
-//             RoundedPolygon(
-//                 vertices = floatArrayOf(0f, -1f, 1f, 1f, 0f, 0.5f, -1f, 1f),
-//                 perVertexRounding =
-//                     listOf(
-//                         CornerRounding(0.2f, 0.5f),
-//                         CornerRounding(0.2f, 0.5f),
-//                         CornerRounding(0.4f, 0f),
-//                         CornerRounding(0.2f, 0.5f),
-//                     ),
-//             )
-//         )
-
-//     @Test
-//     fun quarterAngleMeasure() =
-//         irregularPolygonMeasure(
-//             RoundedPolygon(
-//                 vertices = floatArrayOf(-1f, -1f, 1f, -1f, 1f, 1f, -1f, 1f),
-//                 perVertexRounding =
-//                     listOf(
-//                         CornerRounding.Unrounded,
-//                         CornerRounding.Unrounded,
-//                         CornerRounding(0.5f, 0.5f),
-//                         CornerRounding.Unrounded,
-//                     ),
-//             )
-//         )
-
-//     @Test
-//     fun hourGlassMeasure() {
-//         // Regression test: Legacy measurer (AngleMeasurer) would skip the diagonal sides
-//         // as they are 0 degrees from the center.
-//         val unit = 1f
-//         val coordinates =
-//             floatArrayOf(
-//                 // lower glass
-//                 0f,
-//                 0f,
-//                 unit,
-//                 unit,
-//                 -unit,
-//                 unit,
-
-//                 // upper glass
-//                 0f,
-//                 0f,
-//                 -unit,
-//                 -unit,
-//                 unit,
-//                 -unit,
-//             )
-
-//         val diagonal = sqrt(unit * unit + unit * unit)
-//         val horizontal = 2 * unit
-//         val total = 4 * diagonal + 2 * horizontal
-
-//         val polygon = RoundedPolygon(coordinates)
-//         customPolygonMeasure(
-//             polygon,
-//             floatArrayOf(
-//                 diagonal / total,
-//                 horizontal / total,
-//                 diagonal / total,
-//                 diagonal / total,
-//                 horizontal / total,
-//                 diagonal / total,
-//             ),
-//         )
-//     }
-
-//     @Test
-//     fun handlesEmptyFeatureLast() {
-//         val triangle =
-//             RoundedPolygon(
-//                 listOf(
-//                     Feature.buildConvexCorner(listOf(Cubic.straightLine(0f, 0f, 1f, 1f))),
-//                     Feature.buildConvexCorner(listOf(Cubic.straightLine(1f, 1f, 1f, 0f))),
-//                     Feature.buildConvexCorner(listOf(Cubic.straightLine(1f, 0f, 0f, 0f))),
-//                     // Empty feature at the end.
-//                     Feature.buildConvexCorner(listOf(Cubic.straightLine(0f, 0f, 0f, 0f))),
-//                 )
-//             )
-
-//         irregularPolygonMeasure(triangle)
-//     }
-
-//     private fun regularPolygonMeasure(
-//         sides: Int,
-//         rounding: CornerRounding = CornerRounding.Unrounded,
-//     ) {
-//         irregularPolygonMeasure(RoundedPolygon(sides, rounding = rounding)) { measuredPolygon ->
-//             assertEquals(sides, measuredPolygon.size)
-
-//             measuredPolygon.forEachIndexed { index, measuredCubic ->
-//                 assertEqualish(index.toFloat() / sides, measuredCubic.startOutlineProgress)
-//             }
-//         }
-//     }
-
-//     private fun customPolygonMeasure(polygon: RoundedPolygon, progresses: FloatArray) =
-//         irregularPolygonMeasure(polygon) { measuredPolygon ->
-//             require(measuredPolygon.size == progresses.size)
-
-//             measuredPolygon.forEachIndexed { index, measuredCubic ->
-//                 assertEqualish(
-//                     progresses[index],
-//                     measuredCubic.endOutlineProgress - measuredCubic.startOutlineProgress,
-//                 )
-//             }
-//         }
-
-//     private fun irregularPolygonMeasure(
-//         polygon: RoundedPolygon,
-//         extraChecks: (MeasuredPolygon) -> Unit = {},
-//     ) {
-//         val measuredPolygon = MeasuredPolygon.measurePolygon(measurer, polygon)
-
-//         assertEquals(0f, measuredPolygon.first().startOutlineProgress)
-//         assertEquals(1f, measuredPolygon.last().endOutlineProgress)
-
-//         measuredPolygon.forEachIndexed { index, measuredCubic ->
-//             if (index > 0) {
-//                 assertEquals(
-//                     measuredPolygon[index - 1].endOutlineProgress,
-//                     measuredCubic.startOutlineProgress,
-//                 )
-//             }
-//             assertTrue(measuredCubic.endOutlineProgress >= measuredCubic.startOutlineProgress)
-//         }
-
-//         measuredPolygon.features.forEachIndexed { index, progressableFeature ->
-//             assert(progressableFeature.progress >= 0f && progressableFeature.progress < 1f) {
-//                 "Feature #$index has invalid progress: ${progressableFeature.progress}"
-//             }
-//         }
-
-//         extraChecks(measuredPolygon)
-//     }
-// }
 
 /*
  * Copyright 2023 The Android Open Source Project
